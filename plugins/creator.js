3267ec591ac4a09d30d27e202d6a94ec18bcdc34
nafron <99122428+nafron@users.noreply.github.com>
2022-09-20 07:14:16 +0700

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,16 +20,16 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : WH-MODS-Dev
+${htjava} *💌 Nama* : Muhammad Nafron A.M
 ${htjava} *✉️ Nama RL* : HAMBA ALLAH
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
-${htjava} *⏰ Tanggal lahir* : Private 🥶
-${htjava} *🎨 Umur* : 18
-${htjava} *🧮 Kelas* : SUDAH TAMAT
-${htjava} *🧩 Hobby* : MODIFIKASI WHATSAPP "MODDER WHATSAPP"
-${htjava} *💬 Sifat* : ASIK DAH KALAU DAH KENAL
-${htjava} *🗺️ Tinggal* : Indo, Depok, Jawa Barat
+${htjava} *⏰ Tanggal lahir* : Private 🔒
+${htjava} *🎨 Umur* : 17
+${htjava} *🧮 Kelas* : 2 SMK
+${htjava} *🧩 Hobby* : BERMAIN LAYANGAN "REAL LIFE"
+${htjava} *💬 Sifat* : Bukan bot tapi *FAST RESPON* walaupun begitu selalu di sia-sia in 😓
+${htjava} *🗺️ Tinggal* : Indo, Bondowoso, Jawa Timur
 ${htjava} *❤️ Waifu* : -
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
