7de91ad07308c58a3156b0ec89865bdc4a3dae5c
nafron <99122428+nafron@users.noreply.github.com>
2022-09-21 07:23:38 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,23 +2,19 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://www.youtube.com/channel/UCafhMtYpLNgvoAVDFNrQgdg*
 
-Pengen mentahan nya ?
-Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
-
-Pengen yang udah di recode oleh owner?
+Pengen script ?
 Script bot ini dijual bila kalian mau bisa chat owner
 
-wa.me/6282127487538`
+wa.me/6281252733108`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
-    mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    mediaUrl: "https://Instagram.com/nafronam",
+    mediaType: "Stalk",
+    description: "DEVELOPER FRONBOT", 
+    title: 'FRONBOT RUNNING BY NAFRON',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
